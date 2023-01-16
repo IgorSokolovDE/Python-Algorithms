eed44f75cb18f1f01e12983066a86212b7794178
Igor Sokolov <120279229+IgorSokolovDE@users.noreply.github.com>
2023-01-17 01:00:43 +0200

    Add files via upload

---
+++ Polyakov/5681/5681.py
@@ -0,0 +1,23 @@
+k=0
+for a in 'СПОРТЛ':
+    for b in 'СПОРТЛ':
+        for c in 'СПОРТЛ':
+            for d in 'СПОРТЛ':
+                for e in 'СПОРТЛ':
+                    for f in 'СПОРТЛ':
+                        for g in 'СПОРТЛ':
+                            for h in 'СПОРТЛ':
+                                for i in 'СПОРТЛ':
+                                    slovo=a+b+c+d+e+f+g+h+i
+                                    if 'ООО' in slovo:
+                                        if slovo.count('О')==3:
+                                            if slovo.count('Т')==2:
+                                                if slovo.count('С')==1:
+                                                    if slovo.count('П')==1:
+                                                        if slovo.count('Р')==1:
+                                                            if slovo.count('Л')==1:
+                                                                k+=1
+print(k)
+
+                        
+    
