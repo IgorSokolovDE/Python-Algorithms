12d37fece7f7baeba53f4e4d82cf44c97e326918
Igor Sokolov <120279229+IgorSokolovDE@users.noreply.github.com>
2023-01-22 00:10:52 +0200

    Add files via upload

---
+++ KEGE/6/5661/5661 kege.py
@@ -0,0 +1,18 @@
+import turtle as t
+t.left(90)
+for i in range(7):
+    t.forward(200)
+    t.right(90)
+    t.forward(300)
+    t.right(90)
+t.color('red')
+t.forward(50)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.color('black')
+for i in range(3):
+    t.forward(250)
+    t.right(90)
+    t.forward(150)
+    t.right(90)
