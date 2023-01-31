e776537da2b4d5a7edbb56b5ebc3b677a8f7b1ab
Igor Sokolov <120279229+IgorSokolovDE@users.noreply.github.com>
2023-02-01 00:19:49 +0200

    Add files via upload

--- Polyakov/5928/5928.py
+++ Polyakov/5928/5928.py
@@ -1,15 +1,16 @@
-def Igra(KolKamnei,TekHodIgry,HodPobedy):
-    if KolKamnei>=82:
-        return TekHodIgry%2==HodPobedy%2
-    if TekHodIgry==HodPobedy:
+def F(k,tekHod,hodProing):
+    if k>=82:
+        return tekHod%2==hodProing%2
+    if tekHod==hodProing:
         return False
-    if (TekHodIgry+1)%2==HodPobedy%2:
-        return Igra(KolKamnei+10,TekHodIgry+1,HodPobedy)or Igra(KolKamnei*2,TekHodIgry+1,HodPobedy)
+    if (tekHod+1)%2!=hodProing%2:
+        return F(k+10,tekHod+1,hodProing)or F(k*2,tekHod+1,hodProing)
     else:
-        return Igra(KolKamnei+10,TekHodIgry+1,HodPobedy)and Igra(KolKamnei*2,TekHodIgry+1,HodPobedy)
+        return F(k+10,tekHod+1,hodProing)and F(k*2,tekHod+1,hodProing)
 print('s ход победы')
 for s in range(1,82):
-    for hp in range(1,4):
-        if Igra(s,0,hp):
-            print(s,hp)
+    for hp in range(1,5):
+        if F(s,0,hp):
+            print(s,hp-1)
             break
+    
