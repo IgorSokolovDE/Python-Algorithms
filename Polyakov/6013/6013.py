a21cb8248fa8b9552cc124532287d39265233994
Igor Sokolov <120279229+IgorSokolovDE@users.noreply.github.com>
2023-02-20 00:27:25 +0200

    Add files via upload

---
+++ Polyakov/6013/6013.py
@@ -0,0 +1,9 @@
+print('x y z w F')
+for x in (0,1):    
+    for y in (0,1):
+        for z in (0,1):
+            for w in (0,1):
+                if ((not(y))and(x==(w <= z)))==False:
+                    print(x,y,z,w,'0')
+                
+    
