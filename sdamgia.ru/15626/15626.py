f4ade7e18992fff3a1896ba915b498b18f87201b
Igor Sokolov <120279229+IgorSokolovDE@users.noreply.github.com>
2023-02-23 15:46:14 +0200

    Add files via upload

---
+++ sdamgia.ru/15626/15626.py
@@ -0,0 +1,14 @@
+n=0
+for a1 in 'УОА':
+    for a2 in 'УОА':
+        for a3 in 'УОА':
+            for a4 in 'УОА':
+                for a5 in 'УОА':
+                    for a6 in 'УОА':
+                        n+=1
+                        slovo=a1+a2+a3+a4+a5+a6
+                        if slovo=='ОУУУОО':
+                            print(n)
+                        
+   
+                   
