823f14b70064a1b56c627092ec38965301d398c1
Igor Sokolov <120279229+IgorSokolovDE@users.noreply.github.com>
2023-03-03 00:47:54 +0200

    Add files via upload

--- Polyakov/4725/4725.py
+++ Polyakov/4725/4725.py
@@ -1,26 +1,21 @@
-def F(k1,k2,k3,TekHodIgry,HodPobedy):
+def F(k1,k2,k3,tekHod,HodP):
     if (k1+k2+k3)>=71:
-        return TekHodIgry%2==HodPobedy%2
-    if TekHodIgry==HodPobedy:
+        return tekHod%2==HodP%2
+    if tekHod==HodP:
         return False
     else:
-        if (TekHodIgry+1)%2==HodPobedy%2:
-            return F(k1+3,k2,k3,TekHodIgry+1,HodPobedy)or\
-                   F(k1,k2+3,k3,TekHodIgry+1,HodPobedy)or\
-                   F(k1,k2,k3+3,TekHodIgry+1,HodPobedy)or\
-                   F(k1*2,k2,k3,TekHodIgry+1,HodPobedy)or\
-                   F(k1,k2*2,k3,TekHodIgry+1,HodPobedy)or\
-                   F(k1,k2,k3*2,TekHodIgry+1,HodPobedy)
+        hody=[F(k1+3,k2,k3,tekHod+1,HodP),\
+              F(k1,k2+3,k3,tekHod+1,HodP),\
+              F(k1,k2,k3+3,tekHod+1,HodP),\
+              F(k1*2,k2,k3,tekHod+1,HodP),\
+              F(k1,k2*2,k3,tekHod+1,HodP),\
+              F(k1,k2,k3*2,tekHod+1,HodP)]
+        if (tekHod+1)%2==HodP%2:
+            return any(hody)
         else:
-            return F(k1+3,k2,k3,TekHodIgry+1,HodPobedy)and\
-                   F(k1,k2+3,k3,TekHodIgry+1,HodPobedy)and\
-                   F(k1,k2,k3+3,TekHodIgry+1,HodPobedy)and\
-                   F(k1*2,k2,k3,TekHodIgry+1,HodPobedy)and\
-                   F(k1,k2*2,k3,TekHodIgry+1,HodPobedy)and\
-                   F(k1,k2,k3*2,TekHodIgry+1,HodPobedy)
-print('S ход победы')
-for S in range(1,59):
+            return all(hody)
+for s in range(1,59):
     for hp in range(1,5):
-        if F(7,5,S,0,hp):
-            print(S,hp)
+        if F(7,5,s,0,hp):
+            print(s,hp)
             break
