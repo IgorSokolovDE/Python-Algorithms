175bae94476f4ef2a0c8defe0bbb7d2f0b37f592
Igor Sokolov <120279229+IgorSokolovDE@users.noreply.github.com>
2023-03-04 00:14:53 +0200

    Add files via upload

---
+++ Polyakov/5945/5945.py
@@ -0,0 +1,18 @@
+import turtle as t
+t.left(90)
+for i in range(2):
+    t.forward(60)
+    t.right(90)
+    t.forward(50)
+    t.right(90)
+t.color("red")
+t.back(20)
+t.right(90)
+t.back(30)
+t.left(90)
+t.color("black")
+for i in range(2):
+    t.forward(50)
+    t.right(90)
+    t.forward(60)
+    t.right(90)
