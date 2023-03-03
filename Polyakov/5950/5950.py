c8408e8faf43f6ec60917c87794938b2936a0f3e
Igor Sokolov <120279229+IgorSokolovDE@users.noreply.github.com>
2023-03-04 00:15:39 +0200

    Add files via upload

---
+++ Polyakov/5950/5950.py
@@ -0,0 +1,18 @@
+import turtle as t
+t.left(90)
+for i in range(2):
+    t.forward(70)
+    t.right(90)
+    t.forward(80)
+    t.right(90)
+t.color("red")
+t.forward(20)
+t.right(90)
+t.forward(30)
+t.left(90)
+t.color("black")
+for i in range(2):
+    t.forward(40)
+    t.right(90)
+    t.forward(20)
+    t.right(90)
